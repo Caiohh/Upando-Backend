40bfb4e6ceeb3aeb89cf7538c403996d464150c5
ricardoasgs <ricardoaugustogarcia@hotmail.com>
2022-06-29 14:09:44 -0300

    fix linting

--- src/app.js
+++ src/app.js
@@ -17,12 +17,12 @@ app.get('/', (request, reply) => reply.code(200).send('OK'))
 app.post('/signin', authController.signin)
 app.post('/signup', { preHandler: [checkDuplicateUsernameOrEmail] }, authController.signup)
 
-app.get('/product', { preHandler: [verifyToken] }, productController.getProduct),
-  app.post('/product', { preHandler: [verifyToken] }, productController.postProduct),
-  app.patch('/product/:sku', { preHandler: [verifyToken] }, productController.patchProduct),
-  app.delete('/product/:sku', { preHandler: [verifyToken] }, productController.deleteProduct),
-  app.setErrorHandler(function (error, request, reply) {
-    reply.send(error)
-  })
+app.get('/product', { preHandler: [verifyToken] }, productController.getProduct)
+app.post('/product', { preHandler: [verifyToken] }, productController.postProduct)
+app.patch('/product/:sku', { preHandler: [verifyToken] }, productController.patchProduct)
+app.delete('/product/:sku', { preHandler: [verifyToken] }, productController.deleteProduct)
+app.setErrorHandler(function (error, request, reply) {
+  reply.send(error)
+})
 
 module.exports = app
